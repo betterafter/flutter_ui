91115c5c7d80dbe798411e1f55fa45ad457e2182
keykat <koin12000@naver.com>
2025-05-23 11:22:41 +0900

    feat: 의존성 주입 세팅

---
+++ lib/di.dart
@@ -0,0 +1,15 @@
+import 'package:get_it/get_it.dart';
+
+final getIt = GetIt.instance;
+
+void init() {
+  data();
+  domain();
+  presentation();
+}
+
+void presentation() {}
+
+void domain() {}
+
+void data() {}
